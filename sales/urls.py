624c4347f5610912c2a51c4ff3e5714b727372e9
Surekha Endla <surekhaendla@gmail.com>
2024-12-11 15:49:53 +0530

    added content in templates

--- sales/urls.py
+++ sales/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from sales import views
 
 urlpatterns = [
-    path('',views.login_func),
+    path('',views.login_func, name = 'log'),
     path('signup/',views.signup_func),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,9 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def login_func(request):
-    return render(request,'login.html')
+    if request.method == "POST":
+        name = request.POST["txtname"]
+        email = request.POST["txtemail"]
+        password = request.POST["txtpwd"]
+        user = authenticate(username=name, password=password)
+        if user is not None:
+            if user.is_superuser:
+                return HttpResponse("Welcome to admin page")
+
+            elif user.is_authenticated:
+                return HttpResponse("Welcome to sales page")
+            else:
+                pass
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request,'login.html')
 
 
 def signup_func(request):
-    return render(request,'signup.html')
+    if request.method == "POST":
+        name = request.POST["txtname"]
+        email = request.POST["txtemail"]
+        password = request.POST["txtpwd"]
+
+
+        if User.objects.filter(Q(username = name) | Q(email = email)) & Q(password = password).exists():
+            data = {"msg" : True}
+            return render(request, 'signup.html')
+        else:
+            user1 = User.objects.create_user(username = name, email = email,password = password)
+            user1.save()
+            return redirect('log')
+
+
+
+    else:
+        return render(request,'signup.html')
